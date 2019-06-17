2866fb46cf3bdfeed3ec59e1e27062283d7ea7c4
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 22:44:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,6 @@
 def prime?(num)
   print num
   
-  if num == -1 # neg nums, 1, and 0 are not prime
-    false
-  end
-  
   (2..num - 1).each do |n| # for each num in range between 2 and 1 before num, 
     if num % n == 0
     puts false # if % returns 0, some number other than num and 1 were divisable, a.k.a not prime.

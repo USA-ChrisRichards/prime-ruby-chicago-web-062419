85029030f21560fcb012cf4d44d79813afed1d79
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 22:26:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,4 @@
 # Add  code here!
 def prime?(num)
-  print num
-  range = [0..num]
-  i = 2
-  result = 0
-  while i < num
-    result = num % i
-    i = i + 1
-  end
   
-  if num == 2
-    true
-  elsif (result != 0)
-    false
-  elsif num < 2
-    false
-  end
-   true
 end

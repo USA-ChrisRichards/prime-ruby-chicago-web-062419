26027a410b801c976391b26bf6e6c478b58d3449
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 22:38:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(num)
   if num < 2
-    false
+    puts false
   end
   
   (2..num - 1).each do |n| 
     if num % n == 0
-    false
+    puts false
   end
 end
   true

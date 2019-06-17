3f25404c69bcd93bb8fde3fc1be2dc763e7552aa
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 23:02:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # Add  code here!
-def prime?(value)
-  if value <= 1
+def prime?(num)
+  if num <= 1
     return false
   elsif
     (2..value - 1).each do |n|
-      if value % i == 0
+      if num % n == 0
         return false
       end
     end
   end
-  true  # <= need explanation
+  true
 end
